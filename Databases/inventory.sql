24cac350305f898c41c3fd924e8868c32bb65061
Devison <devisonmhepela07@gmail.com>
2025-12-27 14:31:41 +0530

    This file have schema of all table in inventory.db database

--- Databases/inventory.sql
+++ Databases/inventory.sql
@@ -16,7 +16,6 @@ CREATE TABLE stores (
             symbol TEXT,
             FOREIGN KEY (owner_id) REFERENCES users(id)
         );
-INSERT INTO stores VALUES(1,'2rwkF','Davo store','Gujarati','both retail and wholesale',1,0,1,'e41269450870c97e274bd36458bd294c7127d7a0fbb08a2b10de2243dd0341e4','2025-12-27T11:02:19.430548','india','INR','₹');
 CREATE TABLE users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL UNIQUE,
@@ -39,8 +38,6 @@ CREATE TABLE users (
             FOREIGN KEY (current_store_id) REFERENCES stores(id),
             FOREIGN KEY (current_store_code) REFERENCES stores(store_code)
         );
-INSERT INTO users VALUES(1,'dphilipo','devison','mhepela','philipo','e41269450870c97e274bd36458bd294c7127d7a0fbb08a2b10de2243dd0341e4','boss',NULL,'devisonmhepela07@gmail.com','61 golden vally amoda gujarati vadodara india','2025-12-27T11:02:32.458927',1,'2rwkF',NULL,0,0.0,'monthly',1);
-INSERT INTO users VALUES(2,'dmhepela','devison','philipo','mhepela','e41269450870c97e274bd36458bd294c7127d7a0fbb08a2b10de2243dd0341e4','seller','Davo seller','devison07@gmail.com','61 gujarati vadodara india','2025-12-27T11:11:33.037150',1,'2rwkF','0766878300',0,50000.0,'monthly',1);
 CREATE TABLE user_stores (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER NOT NULL,
@@ -52,8 +49,6 @@ CREATE TABLE user_stores (
             FOREIGN KEY (store_code) REFERENCES stores(store_code),
             UNIQUE(user_id, store_id)
         );
-INSERT INTO user_stores VALUES(1,1,1,'2rwkF',0);
-INSERT INTO user_stores VALUES(2,2,1,'2rwkF',0);
 CREATE TABLE products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             product_code TEXT UNIQUE NOT NULL,
@@ -121,20 +116,15 @@ CREATE TABLE stock_batches (
 CREATE TABLE user_commissions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER NOT NULL,
-            store_code TEXT NOT NULL,
-            commission_rate REAL NOT NULL DEFAULT 0.0,
+            commission_amount REAL NOT NULL DEFAULT 0.0,
             commission_frequency TEXT CHECK(commission_frequency IN ('one_time', 'daily', 'weekly', 'monthly', 'yearly')) DEFAULT 'monthly',
             created_at TEXT NOT NULL DEFAULT (datetime('now')),
             expiry_date DATETIME,
             is_active INTEGER NOT NULL DEFAULT 1,
             synced BOOLEAN DEFAULT 0,
-            FOREIGN KEY (user_id) REFERENCES users(id),
-            FOREIGN KEY (store_code) REFERENCES stores(store_code)
+            FOREIGN KEY (user_id) REFERENCES users(id)
         );
-INSERT INTO user_commissions VALUES(1,2,'2rwkF',0.5,'weekly','2025-12-27 05:41:33','2026-01-03T11:11:33.037083',1,0);
 DELETE FROM sqlite_sequence;
-INSERT INTO sqlite_sequence VALUES('stores',1);
-INSERT INTO sqlite_sequence VALUES('users',2);
-INSERT INTO sqlite_sequence VALUES('user_stores',2);
-INSERT INTO sqlite_sequence VALUES('user_commissions',1);
 COMMIT;
+
+
